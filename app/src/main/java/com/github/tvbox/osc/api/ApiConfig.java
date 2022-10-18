768532cc0da263ba825e86c91844d5f30120254a
wwz09 <wwz5124@gmail.com>
2022-10-18 13:20:12 +0800

    update

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -111,7 +111,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/wwz09/ubuntu/src/branch/master/box1.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/wwz09/ubuntu/raw/branch/master/box1.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
